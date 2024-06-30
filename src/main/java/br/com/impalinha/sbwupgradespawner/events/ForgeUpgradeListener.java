79b9be2d2c83da41fdecf173912c1a5247b9e998
yurisanford <yurisanford@fortestecnologia.com.br>
2024-06-30 17:26:05 -0300

    commit com debu

--- src/main/java/br/com/impalinha/sbwupgradespawner/events/ForgeUpgradeListener.java
+++ src/main/java/br/com/impalinha/sbwupgradespawner/events/ForgeUpgradeListener.java
@@ -3,12 +3,16 @@ package br.com.impalinha.sbwupgradespawner.events;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.screamingsandals.bedwars.api.events.BedwarsUpgradeBoughtEvent;
+import org.screamingsandals.bedwars.api.events.BedwarsUpgradeImprovedEvent;
 
 public class ForgeUpgradeListener implements Listener {
 
     @EventHandler
-    public void onUpgradeBought(BedwarsUpgradeBoughtEvent event) {
-        Bukkit.broadcastMessage("Eis o que você evoluiu: " + event.getStorage().getUpgradeName());
+    public void onUpgradeBought(BedwarsUpgradeImprovedEvent event) {
+        Bukkit.broadcastMessage("LEVEL UP = " + event.getUpgrade().getLevel());
+        Bukkit.broadcastMessage("UPOU PARA O LEVEL = " + event.getNewLevel());
+        Bukkit.broadcastMessage("NOME UPGRADE = " + event.getUpgrade().getName());
+        Bukkit.broadcastMessage("NOME STORAGE = " + event.getStorage().getUpgradeName());
+        Bukkit.broadcastMessage("LEVEL ANTIGO = " + event.getOldLevel());
     }
 }
